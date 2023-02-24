dfbd49949aae6f0c0c822d3ae47c26c160ce7b0f
Kyle Bebeau <kyle@theinitgroup.com>
2023-02-23 23:13:05 -0500

    check browser, add video play promse, and img fallback

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
 import hueMp4 from '../assets/videos/hue.mp4';
 
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
+
 const Homepage = () => {
-  const [year, setYear] = useState();
+  const videoParentRef = useRef();
+  const [shouldUseImage, setShouldUseImage] = useState(false);
+  const [year] = useState((new Date().getFullYear()));
 
   useEffect(() => {
-    setYear((new Date().getFullYear()));
+    if (isSafari() && videoParentRef.current) {
+
+      const player = videoParentRef.current.children[0];
+
+      if (player) {
+        player.controls = false;
+        player.playsinline = true;
+        player.muted = true;
+        player.setAttribute("muted", "");
+        player.autoplay = true;
+
+        setTimeout(() => {
+          const promise = player.play();
+          if (promise.then) {
+            promise
+              .then(() => {})
+              .catch(() => {
+                videoParentRef.current.style.display = "none";
+                setShouldUseImage(true);
+              });
+          }
+        }, 0);
+
+      }
+      
+    }
   }, []);
 
   return (
@@ -14,11 +46,25 @@ const Homepage = () => {
       <header>
         <a href="/community" target="_BLANK">Join Community</a>
       </header>
-      <div className="videoWrap">
-        <video autoPlay muted loop playsInline>
-          <source src={hueMp4} type="video/mp4" />
-        </video>
-      </div>
+      {shouldUseImage ? (
+        <img src={hueMp4} alt="Muted Video" />
+      ) : (
+        <div 
+          ref={videoParentRef}
+          className="videoWrap" 
+          dangerouslySetInnerHTML={{ __html: `
+            <video 
+              loop 
+              muted 
+              autoplay 
+              playsinline
+              preload="metadata"
+            >
+              <source src=${hueMp4} type="video/mp4" />
+            </video>`
+          }}
+        />
+      )}
       <footer>
         <div>©</div>
         <div><span>{year}</span> HUE UNLIMITED</div>
